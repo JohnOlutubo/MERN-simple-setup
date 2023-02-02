773a88808a38c55c733425e191a63e5d13484178
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-02 14:04:06 +0100

    create a main.js file that simply renders the top-level entry React component import React component from HelloWorld.js

---
+++ client/main.js
@@ -0,0 +1,8 @@
+// This main.js file simply renders the top-level entry React component
+// in the div element in the HTML document
+
+import React from "react";
+import { render } from "react-dom";
+import HelloWorld from "./HelloWorld";
+
+render (<HelloWorld/>, document.getElementById("root")) // the entry React component is the HelloWorld component imported from HelloWorld.js:
