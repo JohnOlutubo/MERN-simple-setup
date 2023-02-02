3d353ca36ae15d8f49f233fe0ab3182ca43b317b
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-02 14:07:34 +0100

    create a JS file containing a basic HelloWorld React Component hot-export to enable hot-reloading with react-hot-loader during development

---
+++ client/HelloWorld.js
@@ -0,0 +1,15 @@
+// This file contains a basic HelloWorld React component, which is hot-exported
+// to enable hot reloading with react-hot-loader during development.
+
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default hot(module) (HelloWorld)
