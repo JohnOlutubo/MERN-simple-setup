e340a4f67d2da1c7a9c5086efa0329bd4b24bb1d
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-13 16:14:20 +0100

    add server.js file to set up the server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// this file sets up the server
+
+/* 
+I will implement the Node-Express
+app, which initiates client-side code bundling, starts the server, sets up the path to
+serve static assets to the client, and renders the React view in a template when a
+GET
+request is made to the root route.
+*/
+
+import express from "express";
+import devBundle from "./devBundle";
+
+const app = express();  // I will use this app to build the rest of the Node server application.
+devBundle.compile(app);
