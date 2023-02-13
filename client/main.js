d0809af4d99dcf5ce0f3aed4c0daaa358d49e89f
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2023-02-13 18:20:06 +0100

    main.js file simply renders the top-level entry React component

--- client/main.js
+++ client/main.js
@@ -1,8 +1,5 @@
-// This main.js file simply renders the top-level entry React component
-// in the div element in the HTML document
+import React from 'react'
+import { render } from 'react-dom'
+import HelloWorld from './HelloWorld'
 
-import React from "react";
-import { render } from "react-dom";
-import HelloWorld from "./HelloWorld";
-
-render (<HelloWorld />, document.getElementById("root")) // the entry React component is the HelloWorld component imported from HelloWorld.js:
+render(<HelloWorld/>, document.getElementById('root'))
